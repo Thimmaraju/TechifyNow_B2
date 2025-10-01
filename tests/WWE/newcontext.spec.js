b5b350d061a6330cfd8dd1793a072975807839bd
G Thimmaraju <thimmaraju418@gmail.com>
2025-10-01 20:11:34 +0530

    GitHub actions configured

--- tests/WWE/newcontext.spec.js
+++ tests/WWE/newcontext.spec.js
@@ -17,14 +17,14 @@ test.describe('Automation - Working With Elements', () => {
 
     await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
 
-    await expect.soft(page.locator("//span[text()='Raju']")).toBeVisible()
+  
 
     await page.locator('span').filter({ hasText: 'PIM' }).click();
     await page.locator("//a[text()='Add Employee']").click()
-    await page.getByPlaceholder('First Name').fill(`Yamuna`);
-    await page.getByPlaceholder('Last Name').fill(`T`);
+    await page.getByPlaceholder('First Name').fill(`Akhil`);
+    await page.getByPlaceholder('Last Name').fill(`P`);
     await page.locator('//input[@type="checkbox"]').dispatchEvent('click');
-    await page.locator("//label[text()='Username']/../following-sibling::div/input").fill("yamunattt")
+    await page.locator("//label[text()='Username']/../following-sibling::div/input").fill("akhilppp")
     await page.locator("//label[text()='Password']/../following-sibling::div/input").fill("Pass@1234")
     await page.locator("//label[text()='Confirm Password']/../following-sibling::div/input").fill("Pass@1234")
 
@@ -32,7 +32,7 @@ test.describe('Automation - Working With Elements', () => {
 
     const page2 = await context2.newPage();
     await page2.goto('https://opensource-demo.orangehrmlive.com/');
-    await page2.locator('input[name="username"]').fill("yamunattt")
+    await page2.locator('input[name="username"]').fill("akhilppp")
     await page2.locator("input[type='password']").fill("Pass@1234")
     await page2.locator("input[type='password']").press("Enter")
 
